d36f0dab3b023ce75e7183d2fc85ad3d5e15af9e
Leo Wong <leoelicos@gmail.com>
2022-08-02 01:04:03 +1000

    Create Ingredient Schema

---
+++ server/models/Ingredient.js
@@ -0,0 +1,36 @@
+/*
+ * ingre
+ * server/models/Ingredient.js
+ * This script contains the necessary code to define the Ingredient entity
+ * Copyright 2022 Leo Wong
+ */
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ingredientSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0,
+    default: 0
+  },
+  measure: {
+    type: String
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  }
+});
+
+const Ingredient = mongoose.model('Ingredient', ingredientSchema);
+
+module.exports = Ingredient;
