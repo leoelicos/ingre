3b11ef77105a0957e8632ae1f0d1b4ba681ede2d
Leo Wong <leoelicos@gmail.com>
2022-08-02 05:08:00 +1000

    Change products to recipes

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,61 +1,61 @@
 import React, { useEffect } from 'react';
-import ProductItem from '../ProductItem';
+import RecipeItem from '../RecipeItem';
 import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { UPDATE_RECIPES } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
+import { QUERY_RECIPES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
-function ProductList() {
+function RecipeList() {
   const [state, dispatch] = useStoreContext();
 
   const { currentCategory } = state;
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const { loading, data } = useQuery(QUERY_RECIPES);
 
   useEffect(() => {
     if (data) {
       dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products
+        type: UPDATE_RECIPES,
+        recipes: data.recipes
       });
-      data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
+      data.recipes.forEach((product) => {
+        idbPromise('recipes', 'put', product);
       });
     } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
+      idbPromise('recipes', 'get').then((recipes) => {
         dispatch({
-          type: UPDATE_PRODUCTS,
-          products: products
+          type: UPDATE_RECIPES,
+          recipes: recipes
         });
       });
     }
   }, [data, loading, dispatch]);
 
-  function filterProducts() {
+  function filterRecipes() {
     if (!currentCategory) {
-      return state.products;
+      return state.recipes;
     }
 
-    return state.products.filter((product) => product.category._id === currentCategory);
+    return state.recipes.filter((product) => product.category._id === currentCategory);
   }
 
   return (
     <div className="my-2">
-      <h2>Our Products:</h2>
-      {state.products.length ? (
+      <h2>Our Recipes:</h2>
+      {state.recipes.length ? (
         <div className="flex-row">
-          {filterProducts().map((product) => (
-            <ProductItem key={product._id} _id={product._id} image={product.image} name={product.name} price={product.price} quantity={product.quantity} />
+          {filterRecipes().map((product) => (
+            <RecipeItem key={product._id} _id={product._id} image={product.image} name={product.name} price={product.price} quantity={product.quantity} />
           ))}
         </div>
       ) : (
-        <h3>You haven't added any products yet!</h3>
+        <h3>You haven't added any recipes yet!</h3>
       )}
       {loading ? <img src={spinner} alt="loading" /> : null}
     </div>
   );
 }
 
-export default ProductList;
+export default RecipeList;
