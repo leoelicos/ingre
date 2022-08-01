78d0e9b3a50322dcdc24cb01d8ab4c5e26775d34
Leo Wong <leoelicos@gmail.com>
2022-08-02 01:04:35 +1000

    Create User schema

---
+++ server/models/User.js
@@ -0,0 +1,76 @@
+/*
+ * ingre
+ * server/models/User.js
+ * This script contains the necessary code to define the User entity
+ * Copyright 2022 Leo Wong
+ */
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+const Order = require('./Order');
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  savedRecipes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Recipe',
+      required: true
+    }
+  ],
+  libraryRecipes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Recipe',
+      required: true
+    }
+  ],
+  savedIngredients: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Ingredient',
+      required: true
+    }
+  ],
+  orders: [Order.schema]
+});
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
