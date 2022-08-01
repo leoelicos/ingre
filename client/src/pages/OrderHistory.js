ef8f2f5930d2b672c766971ee3dfcec72df3eff6
Leo Wong <leoelicos@gmail.com>
2022-08-02 05:08:08 +1000

    Change products to recipes

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -15,7 +15,7 @@ function OrderHistory() {
   return (
     <>
       <div className="container my-1">
-        <Link to="/">← Back to Products</Link>
+        <Link to="/">← Back to Recipes</Link>
 
         {user ? (
           <>
@@ -26,9 +26,9 @@ function OrderHistory() {
               <div key={order._id} className="my-2">
                 <h3>{new Date(parseInt(order.purchaseDate)).toLocaleDateString()}</h3>
                 <div className="flex-row">
-                  {order.products.map(({ _id, image, name, price }, index) => (
+                  {order.recipes.map(({ _id, image, name, price }, index) => (
                     <div key={index} className="card px-1 py-1">
-                      <Link to={`/products/${_id}`}>
+                      <Link to={`/recipes/${_id}`}>
                         <img alt={name} src={`/images/${image}`} />
                         <p>{name}</p>
                       </Link>
