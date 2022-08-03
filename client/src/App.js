b13be7b5a42a281eb9007414675494f9e0420f4c
Leo Wong <leoelicos@gmail.com>
2022-08-04 05:42:07 +1000

    Global Font Awesome React Icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Home from './components/Home';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faPen, faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPen, faFloppyDisk);
 
 function App() {
   return <Home />;
