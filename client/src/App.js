04daca45fa0867d46ec7c2a5d576b215da6cc057
Leo Wong <leoelicos@gmail.com>
2022-08-04 07:18:22 +1000

    Import Header. Update fontawesome icons.

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Header from './components/Header';
 
 // allow font awesome icon components to be used globally
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPen, faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
-library.add(faPen, faFloppyDisk);
+import { faBars, faEgg, faCircleInfo, faPen, faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
+library.add(faBars, faEgg, faCircleInfo, faPen, faFloppyDisk);
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div className="page-layout">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
