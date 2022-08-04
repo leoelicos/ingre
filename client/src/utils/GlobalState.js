6927cb48b70c46b6998f217945e4418f53b1aa67
Leo Wong <leoelicos@gmail.com>
2022-08-04 18:50:37 +1000

    Add results to GlobalState

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,13 +1,16 @@
 import React, { createContext, useContext } from 'react';
 import { useGlobalReducer } from './reducers';
+import searchEdamam from '../utils/API';
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
+const recipeSeeds = searchEdamam();
 
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useGlobalReducer({
     collapsed: false,
-    homeRecipes: []
+    homeRecipes: [],
+    results: recipeSeeds
   });
 
   return <Provider value={[state, dispatch]} {...props} />;
