b71d215379a8ba2cdf2ea6c3d38e5f0031f1947a
Leo Wong <leoelicos@gmail.com>
2022-08-04 21:00:49 +1000

    Adjust prop syntax for easier development

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,12 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const RecipeCard = ({ result }) => {
-  console.log('RecipeCard result = ', result);
+const RecipeCard = (props) => {
+  if (!props?.result?.label) console.log('eror, props = ', props);
+
   return (
     <article className="recipe-card">
       <div className="card-header">
         <div className="header-left">
-          <p className="recipe-title">{result.label}</p>
+          <p className="recipe-title">{props.result.label}</p>
+          {/* <p className="recipe-title">recipe.label</p> */}
         </div>
         <div className="header-right">
           <button className="card-title-button">
@@ -18,7 +20,7 @@ const RecipeCard = ({ result }) => {
         </div>
       </div>
       <div className="card-body">
-        <img alt={result.label} src={result.image} className="card-image" />
+        <img alt={props.result.label} src={props.result.image} className="card-image" />
       </div>
     </article>
   );
