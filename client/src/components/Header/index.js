668a7255f07785c8cfa14de257d0bb3a3af89888
Leo Wong <leoelicos@gmail.com>
2022-08-05 21:49:32 +1000

    Implement GlobalState MenuUnfold

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,34 +1,44 @@
 // import { useState } from 'react';
 import { useStoreContext } from '../../utils/state/GlobalState';
-import { SHOW_MODAL } from '../../utils/state/actions';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Col, Row, Button, Steps, Typography } from 'antd';
 import ModalGuide from './ModalGuide';
+import React from 'react';
+import { SHOW_MODAL, TOGGLE_SIDEBAR } from '../../utils/state/actions';
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 const { Step } = Steps;
 const { Title } = Typography;
 
 const App = () => {
-  const [, dispatch] = useStoreContext();
+  const [state, dispatch] = useStoreContext();
+
+  const handleMenuToggle = () => {
+    dispatch({ type: TOGGLE_SIDEBAR });
+  };
 
   const showModal = () => {
     dispatch({ type: SHOW_MODAL });
   };
 
+  const { pathname } = useLocation();
+  const getStep = pathname === '/tapoff' ? 2 : pathname === '/shoppinglist' ? 1 : 0;
+
   return (
     <>
-      <Row align="middle">
-        <Col
-          xs={{ offset: 0, span: 12 }}
-          sm={{ offset: 0, span: 8 }}
-          lg={{ offset: 0, span: 6 }}
+      <Row align="middle" style={{ borderBottom: '1px solid var(--ingre-grey)', paddingBottom: '2px' }}>
+        <Col xs={{ offset: 0, span: 12 }} sm={{ offset: 0, span: 8 }} lg={{ offset: 0, span: 6 }} style={{ width: '1.2rem' }}>
+          <Row align="middle" style={{ marginTop: '-1px', paddingBottom: '2px' }}>
+            {state.leftSidebarCollapsed ? (
+              //
+              <MenuUnfoldOutlined onClick={handleMenuToggle} style={{ color: 'var(--ingre-dark-brown', fontSize: '1.2rem', marginLeft: '1rem' }} />
+            ) : (
+              <MenuFoldOutlined onClick={handleMenuToggle} style={{ color: 'var(--ingre-dark-brown', fontSize: '1.2rem', marginLeft: '1rem' }} />
+            )}
 
-          //
-        >
-          <Row align="middle">
             <Link to="/">
               <Title style={{ marginBottom: 0 }}>
-                <FontAwesomeIcon className="ingre-logo" icon="fa-solid fa-egg" style={{ margin: '0 0.3rem', color: 'var(--ingre-eggshell)' }} />
+                <FontAwesomeIcon className="ingre-logo" icon="fa-solid fa-egg" style={{ margin: '0 0.3rem 0 0.6rem', color: 'var(--ingre-eggshell)', fontSize: '1.8rem', paddingBottom: '2px' }} />
                 <span style={{ color: 'var(--ingre-dark-brown)', fontFamily: 'Poppins, sans-serif', fontSize: '36px', letterSpacing: -1, fontWeight: '800' }}>ingré</span>
               </Title>
             </Link>
@@ -37,28 +47,28 @@ const App = () => {
 
         <Col xs={0} sm={8} lg={0}>
           <Row align="middle">
-            <Steps size="small" current={2} responsive={false}>
+            <Steps size="small" current={getStep} responsive={false}>
               <Step icon={<FontAwesomeIcon icon="fa-solid fa-egg" />} />
               <Step icon={<FontAwesomeIcon icon="fa-solid fa-cart-shopping" />} />
-              <Step icon={<FontAwesomeIcon icon="fa-solid fa-circle" />} />
+              <Step icon={<FontAwesomeIcon icon="fa-solid fa-square-check" />} />
             </Steps>
           </Row>
         </Col>
 
         <Col xs={0} lg={12}>
           <Row align="middle">
-            <Steps size="small" current={2} responsive={false}>
+            <Steps size="small" current={getStep} responsive={false}>
               <Step title="Recipes" icon={<FontAwesomeIcon icon="fa-solid fa-egg" />} />
               <Step title="Shopping List" icon={<FontAwesomeIcon icon="fa-solid fa-cart-shopping" />} />
-              <Step title="Tap Off" icon={<FontAwesomeIcon icon="fa-solid fa-circle" />} />
+              <Step title="Tap Off" icon={<FontAwesomeIcon icon="fa-solid fa-square-check" />} />
             </Steps>
           </Row>
         </Col>
 
         <Col
           xs={{ span: 12, pull: 0 }}
-          sm={{ span: 8, pull: 0 }} /* >576 */
-          lg={{ span: 6, pull: 0 }} /* >576 */
+          sm={{ span: 8, pull: 0 }}
+          lg={{ span: 6, pull: 0 }}
           style={{ height: '100%' }}
           //
         >
