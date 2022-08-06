12a8cd14813ca14e82302d7a2604024f0c5f99c2
Leo Wong <leoelicos@gmail.com>
2022-08-06 20:58:08 +1000

    Comment console.log

--- client/src/components/RecipeCardContainer.js
+++ client/src/components/RecipeCardContainer.js
@@ -3,9 +3,7 @@ import RecipeCard from './RecipeCard';
 const App = (props) => {
   const recipes = props.results.getRecipes;
 
-  for (const recipe of recipes) {
-    console.log(`Recipe name = ${recipe._id}`);
-  }
+  // for (const recipe of recipes) console.log(`Recipe name = ${recipe._id}`);
 
   return (
     <div style={{ display: 'flex', flexFlow: 'row wrap', justifyContent: 'space-evenly' }}>
