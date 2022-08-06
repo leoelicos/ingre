2f6f82418b0773d33aca8ffae3c57f0ac76905cf
Leo Wong <leoelicos@gmail.com>
2022-08-06 23:34:02 +1000

    Move inline styles to higher abstraction

--- client/src/pages/Admin/NoMatch.js
+++ client/src/pages/Admin/NoMatch.js
@@ -1,12 +1,10 @@
-import { Empty } from 'antd';
+import { Empty } from '../../components/Empty';
 
 const App = () => {
   return (
-    <div>
-      <Empty style={{ height: 560, clear: 'both', paddingTop: 120, textAlign: 'center' }}>
-        <h1>Page Not Found</h1>
-      </Empty>
-    </div>
+    <Empty>
+      <h1>Page Not Found</h1>
+    </Empty>
   );
 };
 
