684f8c5947b870711a0f8918ecb5d9c7ecaae8d2
Leo Wong <leoelicos@gmail.com>
2022-08-06 21:49:33 +1000

    Add Divider and Spacing

--- client/src/pages/Admin/Login.js
+++ client/src/pages/Admin/Login.js
@@ -3,23 +3,25 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../../utils/apollo/mutations';
 import Auth from '../../utils/auth';
-import { Button, Form, Input, Space } from 'antd';
-
+import { Button, Form, Input, Space, Divider } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ContentTitle from '../../components/ContentTitle';
 const App = () => {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN);
 
   const handleFormSubmit = async (event) => {
-    console.log('hello');
     event.preventDefault();
     try {
       console.log('logging in with ', formState.email, formState.password);
       const mutationResponse = await login({
-        variables: { email: formState.email, password: formState.password }
+        variables: {
+          email: formState.email,
+          password: formState.password
+          //
+        }
       });
-      console.log('Received mutation response: ', mutationResponse);
       const token = mutationResponse.data.login.token;
-      console.log('Received token: ', token);
       Auth.login(token);
     } catch (e) {
       console.log(e);
@@ -36,9 +38,14 @@ const App = () => {
 
   return (
     <div>
-      <Link to="/signup">← Go to Signup</Link>
-
-      <h2>Login</h2>
+      <Link to="/signup">
+        <Button type="primary">← Go to Signup</Button>
+      </Link>
+      <Divider></Divider>
+      <ContentTitle>
+        Login to <FontAwesomeIcon className="ingre-logo" icon="fa-solid fa-egg" style={{ margin: '0 0.3rem 0 0.6rem', color: 'var(--ingre-eggshell)', fontSize: '1.8rem', paddingBottom: '2px' }} />
+        <span style={{ color: 'var(--ingre-dark-brown)', fontFamily: 'Poppins, sans-serif', fontSize: '36px', letterSpacing: -1, fontWeight: '800' }}>ingré</span>
+      </ContentTitle>
       <Form
         labelCol={{
           span: 6
