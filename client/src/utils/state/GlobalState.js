ac17a6040c6b2db901febd2e869396d40d4c9699
Leo Wong <leoelicos@gmail.com>
2022-08-06 18:56:50 +1000

    Remove backend calls from StoreProvider

--- client/src/utils/state/GlobalState.js
+++ client/src/utils/state/GlobalState.js
@@ -1,17 +1,13 @@
 import React, { createContext, useContext } from 'react';
 import { useGlobalReducer } from './reducers';
-import searchEdamam from '../API';
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
-const recipeSeeds = searchEdamam();
 
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useGlobalReducer({
     modalVisible: false,
-    leftSidebarCollapsed: false,
-    homeRecipes: [],
-    results: recipeSeeds
+    leftSidebarCollapsed: false
   });
 
   return <Provider value={[state, dispatch]} {...props} />;
