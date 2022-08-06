43d20ea50d6ce5b2877dcb9274c575c7662d6c07
Leo Wong <leoelicos@gmail.com>
2022-08-06 23:33:17 +1000

    Define recipe card edit and save buttons (Dummy functions)

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,12 +1,35 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Card, Image } from 'antd';
+import { Card, Image, Button, Space } from 'antd';
 const { Meta } = Card;
+
 const App = (props) => {
+  const handleEdit = () => {
+    console.log('Edit button was clicked');
+  };
+  const handleSave = () => {
+    console.log('Save button was clicked');
+  };
+
   return (
     <Card
       style={{ width: 300, margin: '0.3rem 0.1rem' }}
       cover={<Image width={300} alt={props.name} src={props.picture_url} />}
-      actions={[<FontAwesomeIcon key="edit" icon="fa-solid fa-pen" />, <FontAwesomeIcon key="save" icon="fa-solid fa-floppy-disk" />]}
+      actions={[
+        <Button onClick={handleEdit}>
+          <Space>
+            Edit
+            <FontAwesomeIcon key="edit" icon="fa-solid fa-pen" />
+          </Space>
+        </Button>,
+        <Button onClick={handleSave}>
+          <Space>
+            Save
+            <FontAwesomeIcon key="save" icon="fa-solid fa-floppy-disk" />
+          </Space>
+        </Button>
+
+        //
+      ]}
 
       //
     >
