45c8fec2f279c21d801a3cff048fbc6716b72802
Leo Wong <leoelicos@gmail.com>
2022-08-08 00:10:17 +1000

    Change title on load

--- client/src/pages/Admin/NoMatch.js
+++ client/src/pages/Admin/NoMatch.js
@@ -1,6 +1,10 @@
-import { Empty } from '../../components/Empty';
+import { useEffect } from 'react';
+import Empty from '../../components/Empty';
 
 const App = () => {
+  useEffect(() => {
+    document.title = 'ingré Not Found';
+  }, []);
   return (
     <Empty>
       <h1>Page Not Found</h1>
