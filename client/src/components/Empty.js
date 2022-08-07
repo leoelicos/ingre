584659bd56514658407eddd65819e2255c6265d8
Leo Wong <leoelicos@gmail.com>
2022-08-08 00:09:05 +1000

    Adjusted marginTop

--- client/src/components/Empty.js
+++ client/src/components/Empty.js
@@ -1,7 +1,7 @@
 import { Empty } from 'antd';
 
 const App = (props) => {
-  return <Empty style={{ marginTop: '20vh' }}>{props.children}</Empty>;
+  return <Empty style={{ marginTop: '5vh' }}>{props.children}</Empty>;
 };
 
 export default App;
