8edc8b21ec92849cdff00238fb25b9092415347e
Leo Wong <leoelicos@gmail.com>
2022-08-08 00:13:18 +1000

    Change title on load

--- client/src/pages/ShoppingList.js
+++ client/src/pages/ShoppingList.js
@@ -1,4 +1,7 @@
 const ShoppingList = (props) => {
+  useEffect(() => {
+    document.title = 'ingré Shopping List';
+  }, []);
   return <div className="custom">{props.children}</div>;
 };
 export default ShoppingList;
