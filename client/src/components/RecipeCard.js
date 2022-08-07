c3da305a587ab71f5e458c2374f1f4849db01b97
Leo Wong <leoelicos@gmail.com>
2022-08-08 02:56:31 +1000

    Implement save and edit buttons

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,13 +1,22 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Card, Image, Button, Space } from 'antd';
+import { useStoreContext } from '../utils/state/GlobalState';
+import { ADD_SAVED_RECIPE, ADD_EDIT_RECIPE } from '../utils/state/actions';
+import { Link } from 'react-router-dom';
 const { Meta } = Card;
 
 const App = (props) => {
-  const handleEdit = () => {
-    console.log('Edit button was clicked');
+  const [state, dispatch] = useStoreContext();
+
+  const handleEdit = (event) => {
+    // traverse DOM to get recipe ID
+
+    console.log('Edit button was clicked - the props.recipe is ', props.recipe);
+    dispatch({ type: ADD_EDIT_RECIPE, data: props.recipe });
+    // redirect to Custom
   };
   const handleSave = () => {
-    console.log('Save button was clicked');
+    dispatch({ type: ADD_SAVED_RECIPE, data: props.recipe });
   };
 
   return (
@@ -15,17 +24,26 @@ const App = (props) => {
       style={{ width: 300, margin: '0.3rem 0.1rem' }}
       cover={<Image width={300} alt={props.name} src={props.picture_url} />}
       actions={[
-        <Button onClick={handleEdit}>
-          <Space>
-            Edit
-            <FontAwesomeIcon key="edit" icon="fa-solid fa-pen" />
-          </Space>
-        </Button>,
-        <Button onClick={handleSave}>
-          <Space>
-            Save
-            <FontAwesomeIcon key="save" icon="fa-solid fa-floppy-disk" />
-          </Space>
+        <Link to="/custom">
+          <Button onClick={handleEdit}>
+            <Space>
+              Edit
+              <FontAwesomeIcon key="edit" icon="fa-solid fa-pen" />
+            </Space>
+          </Button>
+        </Link>,
+        <Button onClick={handleSave} disabled={state.savedRecipes.some((r) => r._id === props.recipe._id)}>
+          {state.savedRecipes.some((r) => r._id === props.recipe._id) ? (
+            <Space>
+              Saved!
+              <FontAwesomeIcon key="save" icon="fa-solid fa-floppy-disk" />
+            </Space>
+          ) : (
+            <Space>
+              Save
+              <FontAwesomeIcon key="save" icon="fa-solid fa-floppy-disk" />
+            </Space>
+          )}
         </Button>
 
         //
