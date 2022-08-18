2676a453912bcbbcf7f0ce80da7ec20b3b2b9d7d
Leo Wong <leoelicos@gmail.com>
2022-08-18 17:06:58 +1000

    Uncomment ApolloClient and React Router DOM

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,31 @@
+// React
 import React, { useEffect } from 'react';
-
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Apollo
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-// state
+// Global state provider
 import { StoreProvider } from './utils/state/GlobalState';
 
-// Components
-import Header from './components/Header/index';
-import Layout from './components/Layout.js';
-import SiderLeft from './components/SiderLeft';
-import Content from './components/Content';
-
-// admin
-import Login from './pages/Admin/Login';
-import Signup from './pages/Admin/Signup';
-import NoMatch from './pages/Admin/NoMatch';
-// import Success from './pages/Success';
-// import OrderHistory from './pages/OrderHistory';
-
-// pages
+// Pages
 import Home from './pages/Home';
 import Search from './pages/Search';
 import Custom from './pages/Custom';
 // import Saved from './pages/Saved';
 // import ShoppingList from './pages/ShoppingList';
 // import TapOff from './pages/TapOff';
+
+// Components
+import Header from './components/Header/index';
+import Layout from './components/Layout.js';
+import SiderLeft from './components/SiderLeft';
+import Content from './components/Content';
+import Login from './pages/Admin/Login';
+import Signup from './pages/Admin/Signup';
+import NoMatch from './pages/Admin/NoMatch';
+// import Success from './pages/Success';
 
 // style
 import './App.css';
@@ -39,24 +35,13 @@ import { faBars, faEgg, faCircleInfo, faCookie, faMagnifyingGlass, faCartShoppin
 import { library } from '@fortawesome/fontawesome-svg-core';
 library.add(faBars, faEgg, faCircleInfo, faCookie, faMagnifyingGlass, faCartShopping, faSquareCheck, faPen, faCircleXmark, faAdd, faFloppyDisk, faTrash, faCubesStacked, faRightToBracket, faRightFromBracket, faUserPlus, faTruckLoading);
 
-const httpLink = createHttpLink({
-  uri: '/graphql'
-});
-
+// ApolloClient, cache, and 2 middlewares: authLink and GraphQL
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ''
-    }
-  };
+  return { headers: { ...headers, authorization: token ? `Bearer ${token}` : '' } };
 });
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
-});
+const httpLink = createHttpLink({ uri: '/graphql' });
+const client = new ApolloClient({ link: authLink.concat(httpLink), cache: new InMemoryCache() });
 
 function App() {
   useEffect(() => {
@@ -81,10 +66,7 @@ function App() {
                   <Route path="/saved" element={<Saved />} />
                   <Route path="/shoppinglist" element={<ShoppingList />} />
                   <Route path="/tapoff" element={<TapOff />} />
-                  
-                  
                   <Route path="/success" element={<Success />} />
-                  <Route path="/orderHistory" element={<OrderHistory />} />
                   
                   */}
                   {/* admin */}
