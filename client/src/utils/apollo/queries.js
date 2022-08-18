1426c428e4a9c3629966462691df10ff03835bf1
Leo Wong <leoelicos@gmail.com>
2022-08-18 17:22:15 +1000

    Implement API KEY get safely from backend

--- client/src/utils/apollo/queries.js
+++ client/src/utils/apollo/queries.js
@@ -1,32 +1,67 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
-  {
-    user {
-      firstName
-      lastName
-      orders {
-        _id
-        purchaseDate
-        products {
-          _id
-          name
-          description
-          price
-          quantity
-          image
-        }
-      }
+export const GET_USER_WITH_EMAIL = gql`
+  query GetUserWithEmail($email: String!) {
+    getUserWithEmail(email: $email) {
+      _id
     }
   }
 `;
 
-export const QUERY_RECIPES = gql`
-  query getRecipes {
-    getRecipes {
+export const GET_API_KEY = gql`
+  query GetApiKey {
+    getApiKey {
+      appId
+      appKey
+    }
+  }
+`;
+
+export const GET_USER = gql`
+  query GetUser {
+    getUser {
+      firstName
+      lastName
+    }
+  }
+`;
+
+export const GET_SAVED_RECIPES = gql`
+  query GetSavedRecipes {
+    getSavedRecipes {
       _id
       name
+      portions
       picture_url
+    }
+  }
+`;
+
+export const GET_RECIPE = gql`
+  query GetRecipe($id: ID!) {
+    getRecipe(_id: $id) {
+      _id
+      name
+      portions
+      ingredients {
+        _id
+        name
+        quantity
+        measure
+        category {
+          _id
+          name
+        }
+      }
+      picture_url
+    }
+  }
+`;
+
+export const CHECKOUT = gql`
+  query Checkout {
+    checkout {
+      session
     }
   }
 `;
