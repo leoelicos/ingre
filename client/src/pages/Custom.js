e485412b6e9916040ac70c478dfeded5b414ba1b
Leo Wong <leoelicos@gmail.com>
2022-08-18 17:15:50 +1000

    Fix error - duplicate key for child components

--- client/src/pages/Custom.js
+++ client/src/pages/Custom.js
@@ -6,27 +6,26 @@ import { Button, Form, Input, Space, Col, Divider, Row } from 'antd';
 import ContentTitle from '../components/ContentTitle';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useMutation } from '@apollo/client';
-import { ADD_CUSTOM_RECIPE } from '../utils/apollo/mutations';
+import { SAVE_RECIPE } from '../utils/apollo/mutations';
 import { ADD_SAVED_RECIPE, CLEAR_EDIT_RECIPE } from '../utils/state/actions';
-// import spinner from '../assets/spinner.gif';
+
 const App = (props) => {
   const [state, dispatch] = useStoreContext();
   const [form] = Form.useForm();
-  const [addCustomRecipe, { error }] = useMutation(ADD_CUSTOM_RECIPE);
+  const [addCustomRecipe, { error }] = useMutation(SAVE_RECIPE);
   const [loading, setLoading] = useState(true);
   const [saved, setSaved] = useState(false);
-  const onFinish = async (values) => {
-    // reset custom recipe
-    console.log('Save button was clicked with these values:\n', values);
 
+  const onFinish = async (values) => {
+    console.log('Save button was clicked with these values:\n', values);
     const payload = {
       variables: {
         input: {
           name: values.name || 'Custom recipe',
           portions: parseInt(values.portions),
-          ingredients: values.Ingredient.map((i) => {
+          ingredients: values.ingredients.map((i) => {
             return {
-              name: i.name || 'Ingredient',
+              name: i.name || 'ingredients',
               quantity: parseFloat(i.quantity),
               measure: i.measure || 'unit',
               category: i.category || 'Generic'
@@ -41,7 +40,8 @@ const App = (props) => {
     const mutationResponse = await addCustomRecipe(payload);
     setLoading(false);
     if (error) console.log('error ', error);
-    const data = mutationResponse.data.addCustomRecipe;
+    console.log('mutationResponse =', mutationResponse);
+    const data = mutationResponse.data.saveRecipe;
     console.log('data = ');
     console.log(data);
     dispatch({ type: ADD_SAVED_RECIPE, data });
@@ -62,19 +62,22 @@ const App = (props) => {
       name: state.customRecipe?.name || ''
     });
   };
+
   const initials = () => {
-    const name = state.customRecipe?.name;
-    const portions = parseInt(state.customRecipe?.portions);
-    const Ingredient = state.customRecipe?.ingredients.map((ingredient) => {
-      return {
-        key: ingredient._id,
-        name: ingredient.name || 'Ingredient',
-        quantity: parseFloat(ingredient.quantity || 1),
-        measure: ingredient.measure || 'unit',
-        category: ingredient.category.name || 'Generic'
-      };
-    });
-    const newInitial = { name, portions, Ingredient };
+    const name = state.customRecipe?.name || '';
+    const portions = parseInt(state.customRecipe?.portions) || '';
+    const ingredients =
+      state.customRecipe?.ingredients?.map((ingredient, idx) => {
+        return {
+          key: ingredient._id || ingredient.foodId || 'ingredient' + idx,
+          name: ingredient.name || 'ingredient',
+          quantity: parseFloat(ingredient.quantity || 1),
+          measure: ingredient.measure || 'unit',
+          category: ingredient.category.name || ingredient.category || 'Generic'
+        };
+      }) || [];
+    const newInitial = { name, portions, ingredients };
+    console.log('newInitial =', newInitial);
     // console.log('newInitial = ', newInitial);
     return newInitial;
   };
@@ -82,96 +85,229 @@ const App = (props) => {
   return (
     <>
       <ContentTitle>Custom recipe</ContentTitle>
-      <Form form={form} initialValues={initials()} name="dynamic_form_nest_item" onFinish={onFinish} autoComplete="off">
-        <Col style={{ width: '100%', maxWidth: '800px' }}>
+      <Form
+        form={form}
+        initialValues={initials()}
+        name="dynamic_form_nest_item"
+        onFinish={onFinish}
+        onValuesChange={handleChange}
+        //
+      >
+        <Col span={24} style={{ maxWidth: '800px' }}>
           {/* recipe name and portions */}
           <Row>
-            <Col span={16} style={{ /* background: 'gray', */ maxWidth: '710px' }}>
-              <Divider orientation="left" orientationMargin="4px">
-                Name
-              </Divider>
-
-              <Form.Item name="name" rules={[{ required: true, message: 'Change required' }]}>
-                <Input.TextArea onLoad={setName} onChange={handleChange} placeholder="Enter a name" autoSize={true} style={{ width: '100%' }} />
-              </Form.Item>
-            </Col>
-            <Col span={1}></Col>
-            <Col span={7}>
-              <Divider orientation="left" orientationMargin="4px">
-                Portions
-              </Divider>
-              <Form.Item name="portions" rules={[{ required: true, message: 'Change required' }]}>
-                <Input onChange={handleChange} placeholder="Enter a number" autoSize={true} width="100%" />
-              </Form.Item>
+            <Col span={24}>
+              <Row>
+                <Col span={16}>
+                  <Divider orientation="left" orientationMargin="0">
+                    Name
+                  </Divider>
+                </Col>
+                <Col span={8}>
+                  <Divider orientation="left" orientationMargin="0">
+                    Portions
+                  </Divider>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={16}>
+                  <Form.Item
+                    name="name"
+                    rules={[{ required: true, message: 'Change required' }]}
+                    style={{ marginRight: '4px' }}
+                    //
+                  >
+                    <Input.TextArea
+                      onLoad={setName}
+                      placeholder="Name✏️"
+                      autoSize={true}
+                      allowClear
+                      //
+                    />
+                  </Form.Item>
+                </Col>
+                <Col span={8}>
+                  <Form.Item
+                    name="portions"
+                    rules={[{ required: true, message: 'Change required' }]}
+                    style={{ height: '100%' }}
+                    //
+                  >
+                    <Input.TextArea
+                      placeholder="✏️"
+                      autoSize={true}
+                      allowClear
+                      style={{ height: '100%' }}
+                      //
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
             </Col>
           </Row>
           {/* ingredients */}
-          <Row>
-            <Col span={24}>
-              <Form.List name="Ingredient">
+          <Row key="formbody">
+            <Col key="formwrapper" span={24}>
+              <Form.List name="ingredients">
                 {(fields, { add, remove }) => (
                   <>
                     <Row>
-                      <Col xs={9} sm={9}>
+                      <Col span={6} sm={8}>
                         <Divider orientation="left" orientationMargin="0">
-                          Ingredients
+                          Ingredient
                         </Divider>
                       </Col>
 
-                      <Col xs={0} sm={0} md={2}>
+                      <Col span={5} sm={4}>
                         <Divider orientation="left" orientationMargin="0">
                           Qty
                         </Divider>
                       </Col>
-                      <Col xs={2} sm={2} md={0}>
+                      <Col span={7} sm={4}>
                         <Divider orientation="left" orientationMargin="0">
-                          Q.
+                          Unit
                         </Divider>
                       </Col>
-                      <Col xs={0} sm={0} md={4}>
-                        <Divider orientation="left" orientationMargin="0">
-                          Measure
-                        </Divider>
-                      </Col>
-                      <Col xs={4} sm={4} md={0}>
-                        <Divider orientation="left" orientationMargin="0">
-                          Mea.
-                        </Divider>
-                      </Col>
-                      <Col xs={9} sm={9}>
+                      <Col span={6} sm={8}>
                         <Divider orientation="left" orientationMargin="0">
                           Category
                         </Divider>
                       </Col>
                     </Row>
                     {fields.map((field) => (
-                      <Row key={field.key} align="baseline" style={{ width: '100%' }}>
-                        <Form.Item noStyle shouldUpdate={(prevValues, curValues) => prevValues.name !== curValues.name} style={{}}>
+                      <Row style={{ marginBottom: '4px', height: 'fit-content' }}>
+                        <Form.Item key={'ingredient'} noStyle shouldUpdate={(prevValues, curValues) => prevValues.name !== curValues.name} style={{ marginTop: '1rem' }}>
                           {() => (
-                            <Form.Item {...field} name={[field.name, 'name']} rules={[{ required: true, message: 'Missing name' }]} style={{ width: 'calc((100% - 30px) * 9 / 24)', marginRight: '3px', marginBottom: '0.3rem ', height: '100%' }}>
-                              <Input.TextArea autoSize={true} onChange={handleChange} placeholder="Ingredient name" style={{ padding: '1px' }} />
-                            </Form.Item>
+                            <>
+                              {/* Ingredient name */}
+                              <Col span={6} sm={8}>
+                                <Form.Item
+                                  {...field}
+                                  key="name"
+                                  name={[field.name, 'name']}
+                                  rules={[{ required: true, message: '*' }]}
+                                  style={{ marginRight: '4px', padding: '0px 16px 4px 4px !important' }}
+                                  //
+                                >
+                                  <Input.TextArea
+                                    // allowClear={true}
+                                    autoSize
+                                    placeholder="✏️"
+                                    allowClear
+                                    //
+                                  />
+                                </Form.Item>
+                              </Col>
+
+                              {/* Ingredient quantity */}
+                              <Col span={5} sm={4}>
+                                <Form.Item
+                                  {...field}
+                                  key="quantity"
+                                  name={[field.name, 'quantity']}
+                                  rules={[{ required: true, message: '*' }]}
+                                  style={{ marginRight: '4px' }}
+                                  //
+                                >
+                                  <Input.TextArea
+                                    autoSize={true}
+                                    placeholder="✏️"
+
+                                    //
+                                  />
+                                </Form.Item>
+                              </Col>
+
+                              {/* Ingredient measure */}
+                              <Col span={7} sm={4}>
+                                <Form.Item
+                                  {...field}
+                                  key="measure"
+                                  name={[field.name, 'measure']}
+                                  rules={[{ required: true, message: '*' }]}
+                                  style={{ marginRight: '4px' }}
+
+                                  //
+                                >
+                                  <Input.TextArea
+                                    autoSize
+                                    placeholder="✏️"
+
+                                    //
+                                  />
+                                </Form.Item>
+                              </Col>
+
+                              {/* Ingredient category */}
+                              <Col span={5} sm={7}>
+                                <Form.Item
+                                  {...field}
+                                  key="category"
+                                  name={[field.name, 'category']}
+                                  rules={[{ required: true, message: '*' }]}
+                                  style={{ marginRight: '4px', padding: '0px 16px 4px 4px !important' }}
+                                  //
+                                >
+                                  <Input.TextArea
+                                    autoSize
+                                    placeholder="✏️"
+                                    allowClear
+                                    style={{ padding: '0px 16px 4px 4px !important' }}
+                                    //
+                                  />
+                                </Form.Item>
+                              </Col>
+
+                              {/* Delete ingredient */}
+                              <Col span={1}>
+                                <Form.Item
+                                  key="delete"
+                                  style={{
+                                    borderRadius: '50%',
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    justifyContent: 'center',
+                                    height: '100%'
+                                    //
+                                  }}
+                                >
+                                  <Button
+                                    onClick={() => remove(field.name)}
+                                    style={{
+                                      background: 'var(--ingre-dark-brown)',
+                                      fontSize: 'calc(14px + 0.5vw',
+                                      height: '100%',
+                                      color: 'white',
+                                      padding: '0',
+                                      border: '0',
+                                      width: '100%'
+                                      //
+                                    }}
+                                    //
+                                  >
+                                    <MinusCircleOutlined />
+                                  </Button>
+                                </Form.Item>
+                              </Col>
+                            </>
                           )}
-                        </Form.Item>
-                        <Form.Item {...field} placeholder="Quantity" name={[field.name, 'quantity']} rules={[{ required: true, message: 'Missing quantity' }]} style={{ width: 'calc((100% - 30px) * 2 / 24)', marginRight: '3px', marginBottom: '0.3rem', height: '100%' }}>
-                          <Input.TextArea autoSize={true} onChange={handleChange} placeholder="Qty" style={{ padding: '1px' }} />
-                        </Form.Item>
-                        <Form.Item {...field} placeholder="Measure" name={[field.name, 'measure']} rules={[{ required: true, message: 'Missing measure' }]} style={{ width: 'calc((100% - 30px) * 4 / 24)', marginRight: '3px', marginBottom: '0.3rem', height: '100%' }}>
-                          <Input.TextArea autoSize={true} onChange={handleChange} placeholder="Meas" style={{ padding: '1px' }} />
-                        </Form.Item>
-                        <Form.Item {...field} placeholder="Category" name={[field.name, 'category']} rules={[{ required: true, message: 'Missing category' }]} style={{ width: 'calc((100% - 30px) * 9 / 24)', marginRight: '3px', marginBottom: '0.3rem', height: '100%' }}>
-                          <Input.TextArea autoSize={true} onChange={handleChange} placeholder="Category" style={{ padding: '1px' }} />
-                        </Form.Item>
-                        <Form.Item style={{ borderRadius: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'center', marginBottom: '-4px' }}>
-                          <MinusCircleOutlined onClick={() => remove(field.name)} style={{ color: 'red', fontSize: '1rem', borderRadius: '50%', background: 'white' }} />
                         </Form.Item>
                       </Row>
                     ))}
-                    <Form.Item>
-                      <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />} style={{ maxWidth: '824px' }}>
-                        Add ingredient
-                      </Button>
-                    </Form.Item>
+                    <Row>
+                      <Form.Item style={{ width: '100%', maxWidth: '824px' }}>
+                        <Button
+                          type="dashed"
+                          onClick={() => add()}
+                          block
+                          icon={<PlusOutlined />}
+                          style={{ width: '100%', maxWidth: '824px' }}
+                          //
+                        >
+                          Add ingredient
+                        </Button>
+                      </Form.Item>
+                    </Row>
                   </>
                 )}
               </Form.List>
@@ -182,7 +318,13 @@ const App = (props) => {
               <Form.Item>
                 <Col span={24}>
                   {saved && <Divider>Saved!</Divider>}
-                  <Button disabled={saved} type="primary" htmlType="submit" style={{ width: '100%' }}>
+                  <Button
+                    disabled={saved}
+                    type="primary"
+                    htmlType="submit"
+                    style={{ width: '100%' }}
+                    //
+                  >
                     <Space>
                       <FontAwesomeIcon icon="fa-solid fa-floppy-disk" />
                       Save Recipe
