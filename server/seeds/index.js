3003df6b0d86611fba41e514db7bf0b191ccb8ed
Leo Wong <leoelicos@gmail.com>
2022-08-18 17:28:59 +1000

    Remove all seeds except for seedUsers

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -6,34 +6,20 @@
  */
 
 const db = require('../config/connection');
-const { User, Recipe, Ingredient, Category, Product } = require('../models');
+const { User, Recipe, Ingredient, Category } = require('../models');
 
-const seedCategories = require('./category-seeds');
-const seedIngredients = require('./ingredient-seeds');
-const seedProducts = require('./product-seeds');
-const seedRecipes = require('./recipe-seeds');
 const seedUsers = require('./user-seeds');
 
 // open database
 db.once('open', async () => {
-  // delete database
   await Category.deleteMany();
   await Ingredient.deleteMany();
-  await Product.deleteMany();
   await Recipe.deleteMany();
   await User.deleteMany();
+  console.log('Reset database.');
 
-  console.log('Seeding database…');
-
-  // seed database
-  await seedCategories();
-  await seedIngredients();
-  await seedProducts();
-  await seedRecipes();
   await seedUsers();
+  console.log('Seeded database.');
 
-  console.log('Seeding finished.');
-
-  // exit runtime
   process.exit(0);
 });
