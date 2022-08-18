b5471aef8d4e078f58bf9a1aad3c3f9dd04011d4
Leo Wong <leoelicos@gmail.com>
2022-08-18 17:32:39 +1000

    Refactor for readability

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -9,35 +9,34 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const secret = process.env.HEROKU_JWT_SECRET || process.env.JWT_SECRET;
-console.log('the secret is ', secret);
 const expiration = '2h';
 
 module.exports = {
   authMiddleware: function ({ req }) {
-    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
-
+    // Remove tokenvalue
     // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-
+    if (req.headers.authorization) token = token.split(' ').pop().trim();
     if (!token) {
+      console.error('No token was found!');
       return req;
     }
-
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
-      console.log('Invalid token');
+    } catch (error) {
+      console.log('[authMiddleware] Invalid token');
     }
-
     return req;
   },
   signToken: function ({ firstName, email, _id }) {
     const payload = { firstName, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return jwt.sign(
+      { data: payload },
+      secret,
+      { expiresIn: expiration }
+      //
+    );
   }
 };
