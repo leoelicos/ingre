2e001551202fd5935c437efa908ffa6eaf05ce34
Leo Wong <leoelicos@gmail.com>
2022-08-21 15:25:33 +1000

    Remove most console logs

--- client/src/utils/state/reducers.js
+++ client/src/utils/state/reducers.js
@@ -14,7 +14,7 @@ import {
 
 // The reducer is a function that accepts the current state and an action. It returns a new state based on that action.
 export const reducer = (state, action) => {
-  console.log('action = ', action);
+  console.log(action);
 
   switch (action.type) {
     case SHOW_MODAL:
@@ -23,19 +23,15 @@ export const reducer = (state, action) => {
       return { ...state, modalVisible: false };
 
     case TOGGLE_SIDEBAR:
-      console.log('reducing TOGGLE_SIDEBAR');
       return { ...state, leftSidebarCollapsed: !state.leftSidebarCollapsed };
 
     case UPDATE_SEARCH_RECIPES:
-      console.log(`reducer UPDATE_SEARCH_RECIPES from ${state.searchRecipes.length} items to ${action.data.length} items`);
       return { ...state, searchRecipes: action.data };
 
     case UPDATE_HOME_RECIPES:
-      console.log(`reducer UPDATE_HOME_RECIPES from ${state.homeRecipes.length} items to ${action.data.length} items`);
       return { ...state, homeRecipes: action.data };
 
     case ADD_SAVED_RECIPE:
-      console.log(`reducer ADD_SAVED_RECIPE with this id:\n${action.data._id}`);
       if (state.savedRecipes.find((r) => r._id === action.data._id)) {
         console.log(`Already saved!!`);
         return { ...state };
@@ -46,14 +42,10 @@ export const reducer = (state, action) => {
       return { ...state, savedRecipes: newSavedRecipes };
 
     case ADD_EDIT_RECIPE:
-      console.log(`reducer ADD_EDIT_RECIPE with this recipe:\n${action.data.name}`);
       const newEditRecipe = action.data;
-      console.log('newEditRecipe = ', newEditRecipe);
       return { ...state, customRecipe: newEditRecipe };
 
     case CLEAR_EDIT_RECIPE:
-      console.log(`reducer CLEAR_EDIT_RECIPE  :\n `);
-
       return { ...state, customRecipe: null };
 
     case FLAG_HOME_MOUNTED:
