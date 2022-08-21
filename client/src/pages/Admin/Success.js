e0bc53aebede909438ba7c5c50aacbff1a8de38f
Leo Wong <leoelicos@gmail.com>
2022-08-21 15:13:31 +1000

    Rename idb folder

--- client/src/pages/Admin/Success.js
+++ client/src/pages/Admin/Success.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 
 import { ADD_ORDER } from '../utils/mutations';
-import { idbPromise } from '../../utils/helpers';
+import idbPromise from '../../utils/idb';
 
 function Success() {
   const [addOrder] = useMutation(ADD_ORDER);
