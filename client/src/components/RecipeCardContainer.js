7c8f38e20db86bb447bd22821f0283aab5e6a893
Leo Wong <leoelicos@gmail.com>
2022-08-21 15:12:23 +1000

    Add newlines for legibility. Add conditional rendering

--- client/src/components/RecipeCardContainer.js
+++ client/src/components/RecipeCardContainer.js
@@ -1,19 +1,42 @@
 import RecipeCard from './RecipeCard';
+import { Divider } from 'antd';
 import Empty from './Empty';
 import { v4 as uuidv4 } from 'uuid';
+// Auth
+import Auth from '../utils/auth';
+
 const App = ({ results, loading }) => {
   // console.log('[RecipeCardContainer] results ', results);
 
   return (
-    <div style={{ display: 'flex', flexFlow: 'row wrap', justifyContent: 'space-evenly' }}>
-      {results.length ? (
-        results.map((recipe) => {
-          return <RecipeCard key={uuidv4()} name={recipe.name} picture_url={recipe.picture_url} recipe={recipe} loading={loading} />;
-        })
-      ) : (
-        <Empty />
-      )}
-    </div>
+    <>
+      <div
+        style={{
+          display: 'flex',
+          flexFlow: 'row wrap',
+          justifyContent: 'space-evenly'
+          //
+        }}
+      >
+        {results.length ? !Auth.loggedIn() && <Divider>Log in to edit and save recipes!</Divider> : null}
+        {results.length ? (
+          results.map((recipe) => {
+            return (
+              <RecipeCard
+                key={uuidv4()}
+                name={recipe.name}
+                picture_url={recipe.picture_url}
+                recipe={recipe}
+                loading={loading}
+                //
+              />
+            );
+          })
+        ) : (
+          <Empty />
+        )}
+      </div>
+    </>
   );
 };
 
