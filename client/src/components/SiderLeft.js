e02ed52e6b0329bdbda28e55e31a884e06d50e70
Leo Wong <leoelicos@gmail.com>
2022-08-22 21:38:23 +1000

    Adjust spacing of icons

--- client/src/components/SiderLeft.js
+++ client/src/components/SiderLeft.js
@@ -1,4 +1,4 @@
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Space } from 'antd';
 import { Link } from 'react-router-dom';
 import { useStoreContext } from '../utils/state/GlobalState';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -21,8 +21,11 @@ const App = () => {
       trigger={null}
       collapsible
       collapsed={state.leftSidebarCollapsed}
+      collapsedWidth="60px"
       style={{
-        background: 'var(--ingre-white)'
+        // background: 'var(--ingre-white)',
+        minWidth: '40px !important',
+        background: 'blue'
         //
       }}
     >
@@ -37,71 +40,89 @@ const App = () => {
             key: 1,
             icon: (
               <Link to="/">
-                <FontAwesomeIcon icon="fa-solid fa-cookie" />
+                <FontAwesomeIcon
+                  icon="fa-solid fa-cookie"
+                  style={{ width: '19.19px' }}
+                  //
+                />
               </Link>
             ),
-            label: 'Recipes'
+            label: <Link to="/">Recipes</Link>
           },
           {
             key: 2,
             icon: (
               <Link to="/search">
-                <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+                <FontAwesomeIcon
+                  icon="fa-solid fa-magnifying-glass"
+                  style={{ width: '19.19px' }}
+                  //
+                />
               </Link>
             ),
-            label: 'Search'
+            label: <Link to="/search">Search</Link>
           },
           {
             key: 3,
             icon: (
               <Link to="/custom">
-                <FontAwesomeIcon icon="fa-solid fa-pen" />
+                <FontAwesomeIcon
+                  icon="fa-solid fa-pen"
+                  style={{ width: '19.19px' }}
+                  //
+                />
               </Link>
             ),
-            label: 'Custom'
+            label: <Link to="/custom">Custom</Link>
           },
           {
             key: 4,
             icon: (
               <Link to="/saved">
-                <FontAwesomeIcon icon="fa-solid fa-floppy-disk" />
+                <FontAwesomeIcon
+                  icon="fa-solid fa-floppy-disk"
+                  style={{ width: '19.19px' }}
+                  //
+                />
               </Link>
             ),
             label: (
-              <>
-                Saved{' '}
-                <span
-                  style={{
-                    fontSize: '1rem',
-                    padding: '0 0.4rem',
-                    borderRadius: '50%',
-                    color: 'var(--ingre-white)',
-                    background: 'var(--ingre-dark-brown)'
-                    //
-                  }}
-                >
-                  {state.savedRecipes.length}
-                </span>
-              </>
+              <Link to="/saved">
+                <Space>
+                  Saved
+                  <span
+                    style={{
+                      fontSize: '1rem',
+                      padding: '0 0.4rem',
+                      borderRadius: '50%',
+                      color: 'var(--ingre-white)',
+                      background: 'var(--ingre-dark-brown)'
+                      //
+                    }}
+                  >
+                    {state.savedRecipes.length}
+                  </span>
+                </Space>
+              </Link>
             )
           },
           {
             key: 5,
             icon: (
-              <Link to="/shoppinglist">
-                <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+              <Link to="/shoppinglist" style={{ width: '19.19px', transform: 'translateX(1px)' }}>
+                <FontAwesomeIcon icon="fa-solid fa-egg" />
               </Link>
             ),
-            label: 'Ingredients'
+            label: <Link to="/shoppinglist">Ingredients</Link>
           },
           {
             key: 6,
             icon: (
               <Link to="/tapoff">
-                <FontAwesomeIcon icon="fa-solid fa-square-check" />
+                <FontAwesomeIcon icon="fa-solid fa-square-check" style={{ width: '19.19px' }} />
               </Link>
             ),
-            label: 'Tap'
+            label: <Link to="/tapoff">Tap</Link>
           }
         ]}
       />
