370d7e4617860f0bc7a152c0dc7c3d86cc0e9dbe
Leo Wong <leoelicos@gmail.com>
2022-08-22 21:39:46 +1000

    Replace div with Col

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -2,12 +2,11 @@
 import { useEffect, useState } from 'react';
 
 // Ant components
-import { Form, Input, Cascader } from 'antd';
+import { Form, Input, Cascader, Row, Spin, Divider, Col } from 'antd';
 
 // Custom components
 import RecipeCardContainer from '../components/RecipeCardContainer';
 import ContentTitle from '../components/ContentTitle';
-import ContentSubtitle from '../components/ContentSubtitle';
 
 // Edamam API
 import FetchEdamam from '../utils/api/index.js';
@@ -18,29 +17,21 @@ import { useStoreContext } from '../utils/state/GlobalState';
 // useReducer
 import { UPDATE_SEARCH_RECIPES } from '../utils/state/actions';
 
-// Assets
-import spinner from '../assets/spinner.gif';
-
 // Ant subcomponents
 const { SHOW_CHILD } = Cascader;
 
 function App() {
-  // useContext
+  // Global state
   const [state, dispatch] = useStoreContext();
-  // useState
+
+  // Local state
   const [loadingEdamam, setLoadingEdamam] = useState(false);
   const [edamamRecipes, setEdamamRecipes] = useState(state.searchRecipes);
   const [queryState, setQueryState] = useState('');
-  const [formState, setFormState] = useState({
-    diet: [],
-    health: [],
-    cuisineType: [],
-    mealType: [],
-    dishType: []
-    //
-  });
+  const blankForm = { diet: [], health: [], cuisineType: [], mealType: [], dishType: [] };
+  const [formState, setFormState] = useState(blankForm);
 
-  // useForm
+  // Ant Form hook to enable setFormFields()
   const [form] = Form.useForm();
 
   // Handle search submit
@@ -77,7 +68,6 @@ function App() {
       else if (e[0] === 'dish-type') obj.dishType[obj.dishType.length] = e[1];
     });
     const newState = { ...formState, ...obj };
-    console.log('newState = ', newState);
     setFormState(newState);
   };
 
@@ -86,171 +76,177 @@ function App() {
   }, []);
 
   return (
-    <>
-      <ContentTitle>Search recipes</ContentTitle>
-      <ContentSubtitle>What do you want to cook?</ContentSubtitle>
-      <Form
-        form={form}
-        initialValues={{ remember: true }}
-        onFinish={handleFormSubmit}
-        wrapperCol={24}
-        layout={'vertical'}
-        style={{ maxWidth: '824px' }}
-        //
-      >
-        <Form.Item
-          rules={[{ required: true, message: 'Please input your query!' }]}
+    <Col style={{ width: '100%' }}>
+      <Row>
+        <ContentTitle>Advanced search</ContentTitle>
+      </Row>
+      <Row>
+        <Form
+          form={form}
+          initialValues={{ remember: true }}
+          onFinish={handleFormSubmit}
+          layout={'vertical'}
           style={{
-            marginBottom: '0.3rem'
-            //
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+            maxWidth: '600px'
           }}
-          onChange={handleChange}
+          //
         >
-          <Input.Search
-            name="q"
-            type="text"
-            id="q"
-            value={formState.q}
-            size="middle"
-            enterButton={true}
-            loading={false}
-            onSearch={handleFormSubmit}
-            //
-          />
-        </Form.Item>
+          <Form.Item
+            style={{
+              marginBottom: '0.3rem'
 
-        <Form.Item
-          rules={[
-            {
-              required: true,
-              message: 'Please input your query!'
               //
-            }
-          ]}
-        >
-          <Cascader
-            placeholder="Filters…"
-            options={[
-              {
-                label: 'Diet',
-                value: 'diet',
-                children: [
-                  { label: 'Balanced', value: 'balanced' },
-                  { label: 'High-fiber', value: 'high-fiber' },
-                  { label: 'High-protein', value: 'high-protein' },
-                  { label: 'Low-carb', value: 'low-carb' },
-                  { label: 'Low-fat', value: 'low-fat' },
-                  { label: 'Low-sodium', value: 'low-sodium' }
-                ]
-              },
-              {
-                label: 'Health',
-                value: 'health',
-                children: [
-                  { label: 'Dairy-free', value: 'dairy-free' },
-                  { label: 'Egg-free', value: 'egg-free' },
-                  { label: 'Fish-free', value: 'fish-free' },
-                  { label: 'Gluten-free', value: 'gluten-free' },
-                  { label: 'Low-potassium', value: 'low-potassium' },
-                  { label: 'Low-sugar', value: 'low-sugar' },
-                  { label: 'Lupine-free', value: 'lupine-free' },
-                  { label: 'Mediterranean', value: 'Mediterranean' },
-                  { label: 'Mollusk-free', value: 'mollusk-free' },
-                  { label: 'Mustard-free', value: 'mustard-free' },
-                  { label: 'No-oil-added', value: 'no-oil-added' },
-                  { label: 'Paleo', value: 'paleo' },
-                  { label: 'Peanut-free', value: 'peanut-free' },
-                  { label: 'Pescatarian', value: 'pescatarian' },
-                  { label: 'Pork-free', value: 'pork-free' },
-                  { label: 'Red-meat-free', value: 'red-meat-free' },
-                  { label: 'Sesame-free', value: 'sesame-free' },
-                  { label: 'Shellfish-free', value: 'shellfish-free' },
-                  { label: 'Soy-free', value: 'soy-free' },
-                  { label: 'Sugar-conscious', value: 'sugar-conscious' },
-                  { label: 'Sulfite-free', value: 'sulfite-free' },
-                  { label: 'Tree-nut-free', value: 'tree-nut-free' },
-                  { label: 'Vegan', value: 'vegan' },
-                  { label: 'Vegetarian', value: 'vegetarian' },
-                  { label: 'Wheat-free', value: 'wheat-free' }
-                ]
-              },
-              {
-                label: 'Cuisine',
-                value: 'cuisine-type',
-                children: [
-                  { label: 'American', value: 'American' },
-                  { label: 'Asian', value: 'Asian' },
-                  { label: 'British', value: 'British' },
-                  { label: 'Caribbean', value: 'Caribbean' },
-                  { label: 'Central Europe', value: 'Central Europe' },
-                  { label: 'Chinese', value: 'Chinese' },
-                  { label: 'Eastern Europe', value: 'Eastern Europe' },
-                  { label: 'French', value: 'French' },
-                  { label: 'Indian', value: 'Indian' },
-                  { label: 'Italian', value: 'Italian' },
-                  { label: 'Japanese', value: 'Japanese' },
-                  { label: 'Mediterranean', value: 'Mediterranean' },
-                  { label: 'Mexican', value: 'Mexican' },
-                  { label: 'Middle Eastern', value: 'Middle Eastern' },
-                  { label: 'Nordic', value: 'Nordic' },
-                  { label: 'South American', value: 'South American' },
-                  { label: 'South East Asian', value: 'South East Asian' }
-                ]
-              },
-              {
-                label: 'Meal',
-                value: 'meal-type',
-                children: [
-                  { label: 'Breakfast', value: 'Breakfast' },
-                  { label: 'Dinner', value: 'Dinner' },
-                  { label: 'Lunch', value: 'Lunch' },
-                  { label: 'Snack', value: 'Snack' },
-                  { label: 'Teatime', value: 'Teatime' }
-                ]
-              },
-              {
-                label: 'Dish',
-                value: 'dish-type',
-                children: [
-                  { label: 'Biscuits and cookies', value: 'Biscuits-and-cookies' },
-                  { label: 'Bread', value: 'Bread' },
-                  { label: 'Cereals', value: 'Cereals' },
-                  { label: 'Condiments and sauces', value: 'Condiments-and-sauces' },
-                  { label: 'Desserts', value: 'Desserts' },
-                  { label: 'Drinks', value: 'Drinks' },
-                  { label: 'Main course', value: 'Main-course' },
-                  { label: 'Pancake', value: 'Pancake' },
-                  { label: 'Preps', value: 'Preps' },
-                  { label: 'Preserve', value: 'Preserve' },
-                  { label: 'Salad', value: 'Salad' },
-                  { label: 'Sandwiches', value: 'Sandwiches' },
-                  { label: 'Side dish', value: 'Side dish' },
-                  { label: 'Soup', value: 'Soup' },
-                  { label: 'Starter', value: 'Starter' },
-                  { label: 'Sweets', value: 'Sweets' }
-                ]
-              }
-            ]}
-            onChange={handleFilterChange}
-            multiple
-            maxTagCount="responsive"
-            showCheckedStrategy={SHOW_CHILD}
-            expandTrigger="hover"
-            dropdownClassName="dropdownFilter"
-            //
-          />
-        </Form.Item>
-      </Form>
+            }}
+            onChange={handleChange}
+          >
+            <Input.Search
+              placeholder="Search"
+              name="q"
+              type="text"
+              id="q"
+              value={formState.q}
+              size="large"
+              enterButton={true}
+              loading={false}
+              onSearch={handleFormSubmit}
+              //
+            />
+          </Form.Item>
 
-      {loadingEdamam ? (
-        //
-        <img src={spinner} alt="loading" />
-      ) : (
-        <>
+          <Form.Item>
+            <Cascader
+              placeholder="Filters…"
+              options={[
+                {
+                  label: 'Diet',
+                  value: 'diet',
+                  children: [
+                    { label: 'Balanced', value: 'balanced' },
+                    { label: 'High-fiber', value: 'high-fiber' },
+                    { label: 'High-protein', value: 'high-protein' },
+                    { label: 'Low-carb', value: 'low-carb' },
+                    { label: 'Low-fat', value: 'low-fat' },
+                    { label: 'Low-sodium', value: 'low-sodium' }
+                  ]
+                },
+                {
+                  label: 'Health',
+                  value: 'health',
+                  children: [
+                    { label: 'Dairy-free', value: 'dairy-free' },
+                    { label: 'Egg-free', value: 'egg-free' },
+                    { label: 'Fish-free', value: 'fish-free' },
+                    { label: 'Gluten-free', value: 'gluten-free' },
+                    { label: 'Low-potassium', value: 'low-potassium' },
+                    { label: 'Low-sugar', value: 'low-sugar' },
+                    { label: 'Lupine-free', value: 'lupine-free' },
+                    { label: 'Mediterranean', value: 'Mediterranean' },
+                    { label: 'Mollusk-free', value: 'mollusk-free' },
+                    { label: 'Mustard-free', value: 'mustard-free' },
+                    { label: 'No-oil-added', value: 'no-oil-added' },
+                    { label: 'Paleo', value: 'paleo' },
+                    { label: 'Peanut-free', value: 'peanut-free' },
+                    { label: 'Pescatarian', value: 'pescatarian' },
+                    { label: 'Pork-free', value: 'pork-free' },
+                    { label: 'Red-meat-free', value: 'red-meat-free' },
+                    { label: 'Sesame-free', value: 'sesame-free' },
+                    { label: 'Shellfish-free', value: 'shellfish-free' },
+                    { label: 'Soy-free', value: 'soy-free' },
+                    { label: 'Sugar-conscious', value: 'sugar-conscious' },
+                    { label: 'Sulfite-free', value: 'sulfite-free' },
+                    { label: 'Tree-nut-free', value: 'tree-nut-free' },
+                    { label: 'Vegan', value: 'vegan' },
+                    { label: 'Vegetarian', value: 'vegetarian' },
+                    { label: 'Wheat-free', value: 'wheat-free' }
+                  ]
+                },
+                {
+                  label: 'Cuisine',
+                  value: 'cuisine-type',
+                  children: [
+                    { label: 'American', value: 'American' },
+                    { label: 'Asian', value: 'Asian' },
+                    { label: 'British', value: 'British' },
+                    { label: 'Caribbean', value: 'Caribbean' },
+                    { label: 'Central Europe', value: 'Central Europe' },
+                    { label: 'Chinese', value: 'Chinese' },
+                    { label: 'Eastern Europe', value: 'Eastern Europe' },
+                    { label: 'French', value: 'French' },
+                    { label: 'Indian', value: 'Indian' },
+                    { label: 'Italian', value: 'Italian' },
+                    { label: 'Japanese', value: 'Japanese' },
+                    { label: 'Mediterranean', value: 'Mediterranean' },
+                    { label: 'Mexican', value: 'Mexican' },
+                    { label: 'Middle Eastern', value: 'Middle Eastern' },
+                    { label: 'Nordic', value: 'Nordic' },
+                    { label: 'South American', value: 'South American' },
+                    { label: 'South East Asian', value: 'South East Asian' }
+                  ]
+                },
+                {
+                  label: 'Meal',
+                  value: 'meal-type',
+                  children: [
+                    { label: 'Breakfast', value: 'Breakfast' },
+                    { label: 'Dinner', value: 'Dinner' },
+                    { label: 'Lunch', value: 'Lunch' },
+                    { label: 'Snack', value: 'Snack' },
+                    { label: 'Teatime', value: 'Teatime' }
+                  ]
+                },
+                {
+                  label: 'Dish',
+                  value: 'dish-type',
+                  children: [
+                    { label: 'Biscuits and cookies', value: 'Biscuits-and-cookies' },
+                    { label: 'Bread', value: 'Bread' },
+                    { label: 'Cereals', value: 'Cereals' },
+                    { label: 'Condiments and sauces', value: 'Condiments-and-sauces' },
+                    { label: 'Desserts', value: 'Desserts' },
+                    { label: 'Drinks', value: 'Drinks' },
+                    { label: 'Main course', value: 'Main-course' },
+                    { label: 'Pancake', value: 'Pancake' },
+                    { label: 'Preps', value: 'Preps' },
+                    { label: 'Preserve', value: 'Preserve' },
+                    { label: 'Salad', value: 'Salad' },
+                    { label: 'Sandwiches', value: 'Sandwiches' },
+                    { label: 'Side dish', value: 'Side dish' },
+                    { label: 'Soup', value: 'Soup' },
+                    { label: 'Starter', value: 'Starter' },
+                    { label: 'Sweets', value: 'Sweets' }
+                  ]
+                }
+              ]}
+              allowClear={true}
+              onChange={handleFilterChange}
+              multiple
+              maxTagCount="responsive"
+              showCheckedStrategy={SHOW_CHILD}
+              expandTrigger="hover"
+              dropdownClassName="dropdownFilter"
+              style={{
+                width: 'calc(100% - 64px)'
+                //
+              }}
+              //
+            />
+          </Form.Item>
+        </Form>
+      </Row>
+      <Row>
+        {loadingEdamam ? (
+          //
+          <Divider>
+            <Spin tip="Loading…"></Spin>
+          </Divider>
+        ) : (
           <RecipeCardContainer results={edamamRecipes} loading={loadingEdamam} />
-        </>
-      )}
-    </>
+        )}
+      </Row>
+    </Col>
   );
 }
 
