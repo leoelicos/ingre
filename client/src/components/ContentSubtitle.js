1883884ca9b40351f72827cf2afa409c7004797b
Leo Wong <leoelicos@gmail.com>
2022-08-22 21:37:01 +1000

    Refactor Title props

--- client/src/components/ContentSubtitle.js
+++ client/src/components/ContentSubtitle.js
@@ -3,7 +3,18 @@ const { Title } = Typography;
 
 const App = (props) => (
   <>
-    <Title level={2} style={{ color: 'var(--ingre-blue)', fontFamily: 'Poppins, sans-serif', fontSize: '26px', letterSpacing: -1, fontWeight: '800' }}>
+    <Title
+      level={2}
+      style={{
+        color: 'var(--ingre-blue)',
+        fontFamily: 'Poppins, sans-serif',
+        fontSize: '26px',
+        letterSpacing: -1,
+        fontWeight: '800',
+        margin: '0.3rem 0'
+        //
+      }}
+    >
       {props.children}
     </Title>
   </>
