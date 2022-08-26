ace35eb74c4d0cc60d9aa07cc1edbf40fd75b9d0
Leo Wong <leoelicos@gmail.com>
2022-08-26 18:23:02 +1000

    UPDATE_SAVED_RECIPES, REMOVE_SAVED_RECIPE

--- client/src/utils/state/reducers.js
+++ client/src/utils/state/reducers.js
@@ -5,7 +5,9 @@ import {
   TOGGLE_SIDEBAR,
   UPDATE_SEARCH_RECIPES,
   UPDATE_HOME_RECIPES,
+  UPDATE_SAVED_RECIPES,
   ADD_SAVED_RECIPE,
+  REMOVE_SAVED_RECIPE,
   ADD_EDIT_RECIPE,
   CLEAR_EDIT_RECIPE,
   FLAG_HOME_MOUNTED
@@ -14,7 +16,7 @@ import {
 
 // The reducer is a function that accepts the current state and an action. It returns a new state based on that action.
 export const reducer = (state, action) => {
-  console.log(action);
+  console.log('Reducer', action);
 
   switch (action.type) {
     case SHOW_MODAL:
@@ -30,16 +32,22 @@ export const reducer = (state, action) => {
 
     case UPDATE_HOME_RECIPES:
       return { ...state, homeRecipes: action.data };
+    case UPDATE_SAVED_RECIPES:
+      if (action.data === undefined) return { ...state, savedRecipes: [] };
+      return { ...state, savedRecipes: action.data };
 
     case ADD_SAVED_RECIPE:
-      if (state.savedRecipes.find((r) => r._id === action.data._id)) {
-        console.log(`Already saved!!`);
-        return { ...state };
-      }
-
-      const newSavedRecipes = JSON.parse(JSON.stringify(state.savedRecipes));
+      console.log('state.savedRecipes before', state.savedRecipes);
+      if (state.savedRecipes.find((r) => r._id === action.data._id)) return { ...state };
+      let newSavedRecipes = JSON.parse(JSON.stringify(state.savedRecipes));
       newSavedRecipes.push(action.data);
+      console.log('state.savedRecipes after', newSavedRecipes);
       return { ...state, savedRecipes: newSavedRecipes };
+
+    case REMOVE_SAVED_RECIPE:
+      const copyOfRecipes = JSON.parse(JSON.stringify(state.savedRecipes));
+      const filteredCopyOfRecipes = copyOfRecipes.filter((r) => r._id !== action.data);
+      return { ...state, savedRecipes: filteredCopyOfRecipes };
 
     case ADD_EDIT_RECIPE:
       const newEditRecipe = action.data;
