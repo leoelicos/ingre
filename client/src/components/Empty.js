3a3cc3792cbc37c196885169e91c74ea2cdcb219
Leo Wong <leoelicos@gmail.com>
2022-09-05 21:45:35 +1000

    JSX extension for React Javascript Emmet

--- client/src/components/Empty.js
+++
@@ -1,7 +0,0 @@
-import { Empty } from 'antd';
-
-const App = (props) => {
-  return <Empty style={{ marginTop: '5vh' }} description={props.children}></Empty>;
-};
-
-export default App;
---
+++ client/src/components/Empty.jsx
@@ -0,0 +1,5 @@
+// Ant component
+import { Empty } from 'antd';
+
+const App = (props) => <Empty description={props.children} />;
+export default App;
