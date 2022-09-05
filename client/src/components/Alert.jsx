62490cee03a846d378f7b42ea1c0925010b46885
Leo Wong <leoelicos@gmail.com>
2022-09-05 21:44:51 +1000

    JSX extension for React Javascript Emmet

---
+++ client/src/components/Alert.jsx
@@ -0,0 +1,13 @@
+// Ant component
+import { Alert } from 'antd';
+
+const alertStyle = { margin: '4px 0' };
+
+const App = (props) => (
+  <Alert
+    {...props}
+    style={alertStyle}
+    //
+  />
+);
+export default App;
