06921e43b848a293d6014dee9a45a59915f68528
Leo Wong <leoelicos@gmail.com>
2022-09-05 21:44:55 +1000

    JSX extension for React Javascript Emmet

---
+++ client/src/components/Content.jsx
@@ -0,0 +1,29 @@
+// Ant Components
+import { Layout } from 'antd';
+// components
+import Drawer from './Drawer';
+
+// Ant subcomponents
+const { Content } = Layout;
+
+const contentStyle = {
+  backgroundColor: 'var(--ingre-light-red)',
+  padding: '1rem',
+  display: 'flex',
+  flexFlow: 'column nowrap',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  flex: 1,
+  height: '100%',
+  overflowY: 'auto'
+};
+
+const App = (props) => {
+  return (
+    <Content style={contentStyle}>
+      {props.children}
+      <Drawer />
+    </Content>
+  );
+};
+export default App;
