73b132a3e722a1aa5b05b3c596aa6920eb7267ed
Leo Wong <leoelicos@gmail.com>
2022-09-06 14:58:10 +1000

    useQuery to pass pro status as prop

--- client/src/components/RecipeCardContainer.jsx
+++ client/src/components/RecipeCardContainer.jsx
@@ -2,37 +2,45 @@
 import RecipeCard from './RecipeCard';
 import Empty from './Empty';
 import { BackTop } from 'antd';
+import { useQuery } from '@apollo/client';
+import { GET_USER } from '../utils/apollo/queries';
 
-const App = ({ results, loading, savePage }) => (
-  <>
-    <div
-      style={{
-        display: 'flex',
-        flexFlow: 'row wrap',
-        justifyContent: 'center',
-        marginTop: '1rem',
-        rowGap: '1rem',
-        columnGap: '1rem'
-      }}
-    >
-      {results?.length ? (
-        results.map((recipe, idx) => {
-          return (
-            <RecipeCard
-              key={idx}
-              recipe={recipe}
-              loading={loading}
-              savePage={savePage}
-              //
-            />
-          );
-        })
-      ) : (
-        <Empty>No recipes</Empty>
-      )}
-      <BackTop />
-    </div>
-  </>
-);
+import Auth from '../utils/auth';
+const App = ({ results, loading, savePage }) => {
+  const { data } = useQuery(GET_USER);
+
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          flexFlow: 'row wrap',
+          justifyContent: 'center',
+          marginTop: '1rem',
+          rowGap: '1rem',
+          columnGap: '1rem'
+        }}
+      >
+        {results?.length ? (
+          results.map((recipe, idx) => {
+            return (
+              <RecipeCard
+                key={idx}
+                recipe={recipe}
+                loading={loading}
+                savePage={savePage}
+                pro={Auth.loggedIn() ? data?.getUser?.pro || false : false}
+                //
+              />
+            );
+          })
+        ) : (
+          <Empty>No recipes</Empty>
+        )}
+        <BackTop />
+      </div>
+    </>
+  );
+};
 
 export default App;
