ae5a348f72011cede6986d4658ea9ca465a37bc7
Leo Wong <leoelicos@gmail.com>
2022-09-06 21:03:03 +1000

    useSearchParams to check Stripe token

--- client/src/pages/Success/index.jsx
+++ client/src/pages/Success/index.jsx
@@ -2,47 +2,41 @@
 import { useEffect } from 'react';
 
 // React Router DOM
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 
 // ApolloClient
 import { useMutation } from '@apollo/client';
 import { MAKE_USER_PRO } from '../../utils/apollo/mutations';
-import { Alert, Button, Divider, Empty, Row, Space } from 'antd';
+import { Alert, Button, Col, Divider, Empty, Row, Space } from 'antd';
 
 import { Typography } from 'antd';
 
-// login to update token with pro status
-
-import { LOGIN } from '../../utils/apollo/mutations.js';
 import Auth from '../../utils/auth/index.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const { Title } = Typography;
 
 function Success() {
   const [makeUserPro] = useMutation(MAKE_USER_PRO);
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
-  const [login, { error }] = useMutation(LOGIN);
+  const Token = () => {
+    const params = searchParams.get('session_id');
+
+    console.log('params', params);
+    return null;
+  };
 
   useEffect(() => {
     async function saveOrder() {
       try {
         const product = { membership: 'PRO' };
 
-        const { data } = await makeUserPro({ variables: { product } });
-        const productData = data.addOrder;
-        console.log('productData', productData);
-
-        // need to login again
-        const { email, password } = Auth.getProfile()?.data;
-        const payload = { email, password };
-        const res = await login({ variables: payload });
-        const token = res.data.login.token;
-        console.log('localstorage token', token);
-        Auth.login(token);
+        await makeUserPro({ variables: { product } });
 
         setTimeout(() => {
-          navigate('/');
+          navigate('/upgrade');
         }, 3000);
       } catch (e) {
         console.error(e);
@@ -50,7 +44,7 @@ function Success() {
     }
 
     saveOrder();
-  }, [login, makeUserPro, navigate]);
+  }, [makeUserPro, navigate]);
 
   if (!Auth.loggedIn())
     return (
@@ -65,12 +59,32 @@ function Success() {
       </Empty>
     );
 
-  return error ? (
-    <Alert message={<Space>{JSON.stringify(error)}"Couldn't make you PRO. Please email admin@ingre.com for assistance."</Space>} type="error" />
-  ) : (
+  if (!searchParams.get('session_id')) {
+    return (
+      <Col>
+        <Row>
+          <Alert message="Payment error" type="error" />
+        </Row>
+        <Row>
+          <Button type="primary" shape="round" block>
+            <Link to="/upgrade">
+              <FontAwesomeIcon
+                icon="fa-solid fa-book-open"
+                style={{
+                  width: '19.19px'
+                }}
+              />
+              Try again?
+            </Link>
+          </Button>
+        </Row>
+      </Col>
+    );
+  }
+  return (
     <Row>
       <Space direction="vertical">
-        <Title level={1}>Success!</Title>
+        <Title level={1}>Success!{<Token />}</Title>
         <Title level={2}>Thank you for your purchase!</Title>
         <Title level={3}>You will now be redirected to the home page.</Title>
       </Space>
