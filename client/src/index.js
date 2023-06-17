aece8d1eb82916b25cd7fa10df6f108e00748856
Leo Wong <leoelicos@gmail.com>
2023-06-17 23:58:20 +1000

    Update App name

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
+import Ingre from './Ingre.tsx'
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
