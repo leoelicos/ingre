a5c94db7031434a3ac96373b11e540b5ce9588cc
Leo Wong <leoelicos@gmail.com>
2023-06-18 14:26:05 +1000

    Separate UI from logic

--- client/src/components/RecipeCardContainer.jsx
+++ client/src/components/RecipeCardContainer.jsx
@@ -1,46 +1,61 @@
-// Custom components
 import RecipeCard from './RecipeCard'
-
-import { BackTop, Empty } from 'antd'
+import { BackTop, Divider, Empty, Spin } from 'antd'
 import { useQuery } from '@apollo/client'
 import { GET_USER } from '../utils/apollo/queries'
-
 import Auth from '../utils/auth'
-const App = ({ results, loading, savePage }) => {
+import { useMemo } from 'react'
+
+const RecipeCardContainer = ({ results, loading, onSavedPage }) => {
   const { data } = useQuery(GET_USER)
 
-  return (
-    <>
-      <div
-        style={{
-          display: 'flex',
-          flexFlow: 'row wrap',
-          justifyContent: 'center',
-          marginTop: '1rem',
-          rowGap: '1rem',
-          columnGap: '1rem'
-        }}
-      >
-        {results?.length ? (
-          results.map((recipe, idx) => {
-            return (
-              <RecipeCard
-                key={idx}
-                recipe={recipe}
-                loading={loading}
-                savePage={savePage}
-                pro={Auth.loggedIn() ? data?.getUser?.pro || false : false}
-                //
-              />
-            )
-          })
-        ) : (
-          <Empty>No recipes</Empty>
-        )}
+  const memoizedResults = useMemo(
+    () =>
+      results?.map((recipe, idx) => {
+        return (
+          <RecipeCard
+            key={idx}
+            recipe={recipe}
+            onSavedPage={onSavedPage}
+            pro={Auth.loggedIn() ? data?.getUser?.pro || false : false}
+          />
+        )
+      }),
+    [results, data, onSavedPage]
+  )
+
+  if (loading) {
+    return <RecipeCardContainerLoading />
+  } else if (!results || !results.length) {
+    return <RecipeCardContainerEmpty />
+  } else {
+    return (
+      <div style={recipeCardContainerStyle}>
+        {memoizedResults}
         <BackTop />
       </div>
-    </>
-  )
+    )
+  }
 }
 
-export default App
+export default RecipeCardContainer
+
+const recipeCardContainerStyle = {
+  display: 'flex',
+  flexFlow: 'row wrap',
+  justifyContent: 'center',
+  marginTop: '1rem',
+  rowGap: '1rem',
+  columnGap: '1rem'
+}
+
+const RecipeCardContainerLoading = () => (
+  <Divider>
+    <Spin tip="Loading saved recipes…" />
+  </Divider>
+)
+const RecipeCardContainerEmpty = () => (
+  <div style={recipeCardContainerStyle}>
+    <Empty>No recipes</Empty>
+    <BackTop />
+  </div>
+)
