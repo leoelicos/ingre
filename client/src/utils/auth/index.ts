b917ae97b3500ebf68d3b4a5944121333df449fc
Leo Wong <leoelicos@gmail.com>
2023-06-20 13:02:41 +1000

    Added types

--- client/src/utils/auth/index.ts
+++ client/src/utils/auth/index.ts
@@ -2,32 +2,40 @@
 
 import decode from 'jwt-decode'
 
+type profileType = {
+  data: { firstName: string; email: string; _id: string; pro: string }
+  exp: number
+}
+
 // Singleton class for authentication
 class AuthService {
-  getProfile = () => {
+  getProfile(): profileType | null {
     const token = this.getToken()
-    return token ? decode(this.getToken()) : null
+    const profile = token ? decode<profileType>(this.getToken()) : null
+    return profile
   }
 
-  getToken = () => localStorage.getItem('id_token')
+  getToken(): string {
+    return localStorage.getItem('id_token') || ''
+  }
 
-  isTokenExpired = (token) => {
-    const decoded = decode(token)
+  isTokenExpired(token: string): boolean {
+    const decoded = decode<profileType>(token)
     const { exp: timeExpiry } = decoded
     const timeNow = Date.now() / 1000
     return timeExpiry < timeNow
   }
 
-  loggedIn = () => {
+  loggedIn(): boolean {
     const token = this.getToken()
     return !!token && !this.isTokenExpired(token)
   }
 
-  login(idToken) {
+  login(idToken: string): void {
     localStorage.setItem('id_token', idToken)
   }
 
-  logout() {
+  logout(): void {
     localStorage.removeItem('id_token')
   }
 }
