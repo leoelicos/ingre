b2670b67627c849832a90942a0489636a5c60631
Leo Wong <leoelicos@gmail.com>
2023-06-23 21:17:23 +1000

    Move component into Authentication folder

---
+++ client/src/components/Authentication/NotLoggedIn.tsx
@@ -0,0 +1,15 @@
+import { Divider, Empty, Row } from 'antd'
+import React, { FC } from 'react'
+import LoginLink from './LoginLink.tsx'
+
+const NotLoggedIn: FC = () => {
+  return (
+    <Empty>
+      <Divider />
+      <Row>You need to be logged in to see this page.</Row>
+      <LoginLink />
+    </Empty>
+  )
+}
+
+export default NotLoggedIn
