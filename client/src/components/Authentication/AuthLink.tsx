7d5452360ddd74b68a9a9193651f48eed48de8f1
Leo Wong <leoelicos@gmail.com>
2023-06-25 19:07:46 +1000

    Add styling. Add text prop

--- client/src/components/Authentication/AuthLink.tsx
+++ client/src/components/Authentication/AuthLink.tsx
@@ -1,11 +1,20 @@
 /* react */
-import React, { FC, ReactNode } from 'react'
+import React, { FC, CSSProperties } from 'react'
 import { Link } from 'react-router-dom'
 
-const AuthLink: FC<{ link: string; children: ReactNode }> = ({
-  link,
-  children
-}) => {
-  return <Link to={link}>{children}</Link>
+const style: CSSProperties = {
+  color: 'var(--ingre-dark-brown)',
+  fontSize: '1.2rem',
+  margin: '1rem 1.2rem 0 1.2rem',
+  padding: '0 8px',
+  borderRadius: '1.2rem'
+}
+
+const AuthLink: FC<{ link: string; text: string }> = ({ link, text }) => {
+  return (
+    <Link to={link}>
+      <div style={style}>{text}</div>
+    </Link>
+  )
 }
 export default AuthLink
