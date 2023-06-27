eec6365a04bd0a3987a3f3ac63c10551fbc16de7
Leo Wong <leoelicos@gmail.com>
2023-06-27 20:24:44 +1000

    Remove unused code

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Ingre from './components/Ingre.tsx'
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-// import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container)
 
-root.render(
-  // temporarily disabled React.StrictMode for production
-  <Ingre />
-)
-
-// serviceWorkerRegistration.register();
-// reportWebVitals();
+root.render(<Ingre />)
