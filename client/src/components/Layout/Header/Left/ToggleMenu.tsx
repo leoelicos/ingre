62c6fb65bae38e2b44398fd65140a9178c15338f
Leo Wong <leoelicos@gmail.com>
2023-06-28 23:41:36 +1000

    Remove unused imports. import Logo component

--- client/src/components/Layout/Header/Left/ToggleMenu.tsx
+++ client/src/components/Layout/Header/Left/ToggleMenu.tsx
@@ -3,10 +3,9 @@ import React, { FC } from 'react'
 import { Link } from 'react-router-dom'
 
 /* components */
-import { Col, Row, Typography } from 'antd'
+import { Col, Row } from 'antd'
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import Logo from './Logo.tsx'
 
 /* types */
 interface ToggleMenuProps {
@@ -42,29 +41,7 @@ const ToggleMenu: FC<ToggleMenuProps> = ({ collapsed, onClick }) => (
       )}
 
       <Link to="/">
-        <Typography.Title style={{ marginBottom: 0 }}>
-          <FontAwesomeIcon
-            className="ingre-logo"
-            icon={'fa-solid fa-egg' as IconProp}
-            style={{
-              marginRight: '0.3rem',
-              color: 'var(--ingre-eggshell)',
-              fontSize: '1.8rem',
-              paddingBottom: '2px'
-            }}
-          />
-          <span
-            style={{
-              color: 'var(--ingre-dark-brown)',
-              fontFamily: 'Poppins, sans-serif',
-              fontSize: '36px',
-              letterSpacing: -1,
-              fontWeight: '800'
-            }}
-          >
-            ingré
-          </span>
-        </Typography.Title>
+        <Logo />
       </Link>
     </Row>
   </Col>
