82365bd56c377abd398292413cb180beee481c0d
Leo Wong <leoelicos@gmail.com>
2023-06-30 01:29:42 +1000

    Add type for payloads

---
+++ client/src/@types/payloads.d.ts
@@ -0,0 +1,15 @@
+export interface SaveRecipePayloadType {
+  name: string
+  portions: number
+  ingredients: IngredientInput[]
+  picture_url: string
+  edamamId: string
+  instructions: string
+}
+
+interface IngredientInput {
+  name: string
+  quantity: number
+  measure: string
+  category: string
+}
