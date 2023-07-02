c9ced99da81d4419be4d83c573f6f17dcbc7d0a1
Leo Wong <leoelicos@gmail.com>
2023-07-02 20:01:35 +1000

    improve code readability

--- client/src/components/Layout/Header/Left/Logo.tsx
+++ client/src/components/Layout/Header/Left/Logo.tsx
@@ -1,33 +1,51 @@
-import React, { FC } from 'react'
+import React, { CSSProperties, FC } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Typography } from 'antd'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 
+const { Title } = Typography
+const LogoIconStyle: CSSProperties = {
+  marginRight: '0.3rem',
+  color: 'var(--ingre-eggshell)',
+  fontSize: '1.8rem',
+  paddingBottom: '2px'
+}
+
+function LogoIcon() {
+  return (
+    <FontAwesomeIcon
+      className="ingre-logo-icon"
+      icon={'fa-solid fa-egg' as IconProp}
+      style={LogoIconStyle}
+    />
+  )
+}
+
+const LogoTextStyle: CSSProperties = {
+  color: 'var(--ingre-dark-brown)',
+  fontFamily: 'Poppins, sans-serif',
+  fontSize: '36px',
+  letterSpacing: -1,
+  fontWeight: '800'
+}
+
+function LogoText() {
+  return (
+    <span
+      className="ingre-log-text"
+      style={LogoTextStyle}
+    >
+      ingré
+    </span>
+  )
+}
+
 const Logo: FC = () => {
   return (
-    <Typography.Title style={{ marginBottom: 0 }}>
-      <FontAwesomeIcon
-        className="ingre-logo"
-        icon={'fa-solid fa-egg' as IconProp}
-        style={{
-          marginRight: '0.3rem',
-          color: 'var(--ingre-eggshell)',
-          fontSize: '1.8rem',
-          paddingBottom: '2px'
-        }}
-      />
-      <span
-        style={{
-          color: 'var(--ingre-dark-brown)',
-          fontFamily: 'Poppins, sans-serif',
-          fontSize: '36px',
-          letterSpacing: -1,
-          fontWeight: '800'
-        }}
-      >
-        ingré
-      </span>
-    </Typography.Title>
+    <Title style={{ marginBottom: 0 }}>
+      <LogoIcon />
+      <LogoText />
+    </Title>
   )
 }
 export default Logo
