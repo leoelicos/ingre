fd5e09bf2f908ee44a1ea4faef767741c87d6cc2
Leo Wong <leoelicos@gmail.com>
2023-07-03 13:55:03 +1000

    Extract component to Icon.tsx

--- client/src/components/Layout/Header/Left/Logo.tsx
+++ client/src/components/Layout/Header/Left/Logo.tsx
@@ -1,25 +1,8 @@
 import React, { CSSProperties, FC } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Typography } from 'antd'
-import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { IngreIconLogoEgg } from '../../../Icons/Icon.tsx'
 
 const { Title } = Typography
-const LogoIconStyle: CSSProperties = {
-  marginRight: '0.3rem',
-  color: 'var(--ingre-eggshell)',
-  fontSize: '1.8rem',
-  paddingBottom: '2px'
-}
-
-function LogoIcon() {
-  return (
-    <FontAwesomeIcon
-      className="ingre-logo-icon"
-      icon={'fa-solid fa-egg' as IconProp}
-      style={LogoIconStyle}
-    />
-  )
-}
 
 const LogoTextStyle: CSSProperties = {
   color: 'var(--ingre-dark-brown)',
@@ -43,7 +26,7 @@ function LogoText() {
 const Logo: FC = () => {
   return (
     <Title style={{ marginBottom: 0 }}>
-      <LogoIcon />
+      <IngreIconLogoEgg />
       <LogoText />
     </Title>
   )
