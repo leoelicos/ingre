ec6cb5a4c60cf9c0434402fda303b18c5e48a130
Leo Wong <leoelicos@gmail.com>
2023-07-10 20:35:12 +1000

    Moved from RecipeCard

---
+++ client/src/components/Recipe/components/EditButton.tsx
@@ -0,0 +1,32 @@
+import { Button, Tooltip } from 'antd'
+import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
+import { IngreIconCustomise } from '../../Icons/Icon.tsx'
+
+const EditButton: FC<{
+  getRecipeLoading: boolean // load from server
+  handleEdit: any
+  getRecipeError: any // load from server
+}> = ({ getRecipeLoading, handleEdit, getRecipeError }) => (
+  <Tooltip
+    color="var(--ingre-dark-brown)"
+    placement="top"
+    title="Edit"
+    className="button-tooltip"
+  >
+    <Button
+      loading={getRecipeLoading}
+      disabled={getRecipeError}
+      onClick={handleEdit}
+      style={{
+        borderRadius: '50%',
+        padding: '4px 8px'
+      }}
+    >
+      <Link to="/customise">
+        <IngreIconCustomise />
+      </Link>
+    </Button>
+  </Tooltip>
+)
+export default EditButton
