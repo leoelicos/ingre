3d7d2d4ee95c2f9af480d4ddf9f6a15545f8f8ac
Leo Wong <leoelicos@gmail.com>
2023-07-10 20:35:03 +1000

    Moved from RecipeCard

---
+++ client/src/components/Recipe/components/DisabledEditButton.tsx
@@ -0,0 +1,26 @@
+import { Button, Tooltip } from 'antd'
+import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
+import { IngreIconCustomise } from '../../Icons/Icon.tsx'
+
+const DisabledEditButton: FC = () => (
+  <Tooltip
+    placement="top"
+    title={
+      <Link to="/login">
+        <Button type="primary">Log in</Button>
+      </Link>
+    }
+  >
+    <Button
+      disabled
+      style={{
+        borderRadius: '50%',
+        padding: '4px 8px'
+      }}
+    >
+      <IngreIconCustomise />
+    </Button>
+  </Tooltip>
+)
+export default DisabledEditButton
