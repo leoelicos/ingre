2156d1afd80faac3780287066c9ac4c53e885e5e
Leo Wong <leoelicos@gmail.com>
2023-07-12 21:18:11 +1000

    remove optional types

--- client/src/@types/recipe.d.ts
+++ client/src/@types/recipe.d.ts
@@ -2,12 +2,12 @@
 import type { IngredientType } from './ingredient.d.ts'
 
 export interface RecipeType {
-  _id?: string // from server
-  name?: string
-  portions?: number
-  picture_url?: string // may not have one
-  instructions?: string // may not have one
-  shareAs?: string // will not have one if created from scratch
-  edamamId?: string // may not have one if created from scratch
+  _id: string | undefined // will not have one if not from server
+  name: string
+  portions: number
+  picture_url: string
+  instructions: string
+  shareAs: string
+  edamamId: string
   ingredients: IngredientType[]
 }
