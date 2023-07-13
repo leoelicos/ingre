d6e3295df9c9c48c629c5b2bb7ac0cf908c2e52f
Leo Wong <leoelicos@gmail.com>
2023-07-13 20:56:32 +1000

    Remove unused code

--- client/src/@types/search.d.ts
+++
@@ -1,9 +0,0 @@
-/* from the search page */
-export interface SearchParams {
-  q?: string
-  diet?: string[]
-  health?: string[]
-  cuisineType?: string[]
-  mealType?: string[]
-  dishType?: string[]
-}
