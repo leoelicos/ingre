c6cf3e8f138523c2b58fd5fe59aac6253c972d29
Leo Wong <leoelicos@gmail.com>
2023-07-13 20:56:23 +1000

    Remove unused code

--- client/src/@types/ingredient.d.ts
+++
@@ -1,9 +0,0 @@
-import type { CategoryType } from './category.d.ts'
-
-export interface IngredientType {
-  _id?: string
-  name: string
-  quantity: number
-  measure: string
-  category: CategoryType
-}
