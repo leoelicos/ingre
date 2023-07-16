5a15e36bab5bf50f0453cbc7697e8513b02bc68d
Leo Wong <leoelicos@gmail.com>
2023-07-16 22:30:30 +1000

    Update import link

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Ingre from './components/Ingre.tsx'
+import Ingre from './App.tsx'
 
 const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container)
