a574c624b9b6f7df3142586c85c0069dca77cb1b
Leo Wong <leoelicos@gmail.com>
2023-07-16 22:30:43 +1000

    Add LoginLink

--- client/src/pages/Admin/Logout.tsx
+++ client/src/pages/Admin/Logout.tsx
@@ -3,11 +3,18 @@ import React, { FC, useEffect, useState } from 'react'
 
 /* state */
 import { useAuthContext } from '../../utils/auth/AuthContext.tsx'
+import LoginLink from '../../components/Links/LoginLink.tsx'
 
 const Logout: FC = () => {
   const [state, dispatch] = useAuthContext()
   const loggedIn = state.loggedIn
-  if (!loggedIn) return <div>You are already logged out.</div>
+  if (!loggedIn)
+    return (
+      <div>
+        <div>You are logged out.</div>
+        <LoginLink />
+      </div>
+    )
 
   const [m, setM] = useState('Logging out…')
 
