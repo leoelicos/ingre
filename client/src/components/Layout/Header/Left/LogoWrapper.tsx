551662389584016ed20bb85c59c2e80583d204b9
Leo Wong <leoelicos@gmail.com>
2023-07-28 17:37:32 +1000

    Destructure ReactNode

--- client/src/components/Layout/Header/Left/LogoWrapper.tsx
+++ client/src/components/Layout/Header/Left/LogoWrapper.tsx
@@ -1,7 +1,7 @@
 import { Typography } from 'antd'
-import React, { FC } from 'react'
+import React, { FC, ReactNode } from 'react'
 
-const LogoWrapper: FC<{ children: React.ReactNode }> = ({ children }) => (
+const LogoWrapper: FC<{ children: ReactNode }> = ({ children }) => (
   <Typography.Title style={{ marginBottom: 0 }}>{children}</Typography.Title>
 )
 
