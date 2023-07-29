b53b014a451b8f44d49eea19a13362f37f3eb37d
Leo Wong <leoelicos@gmail.com>
2023-07-29 20:26:12 +1000

    Define style

--- client/src/components/Layout/Header/Left/LogoWrapper.tsx
+++ client/src/components/Layout/Header/Left/LogoWrapper.tsx
@@ -1,8 +1,11 @@
 import { Typography } from 'antd'
-import React, { FC, ReactNode } from 'react'
+import React, { CSSProperties, FC, ReactNode } from 'react'
 
+const style: CSSProperties = {
+  marginBottom: 0
+}
 const LogoWrapper: FC<{ children: ReactNode }> = ({ children }) => (
-  <Typography.Title style={{ marginBottom: 0 }}>{children}</Typography.Title>
+  <Typography.Title style={style}>{children}</Typography.Title>
 )
 
 export default LogoWrapper
