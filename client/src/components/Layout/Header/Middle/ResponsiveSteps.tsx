fa6745edf94edd231d99bffb5862960e25b1b119
Leo Wong <leoelicos@gmail.com>
2023-08-02 16:19:08 +1000

    Combine StepBig and StepSmall together

---
+++ client/src/components/Layout/Header/Middle/ResponsiveSteps.tsx
@@ -0,0 +1,41 @@
+/* react */
+import React, { FC } from 'react'
+
+/* components */
+import { Col, Row } from 'antd'
+import Steps from './Steps/Steps.tsx'
+
+const ResponsiveSteps: FC<{ step: number }> = ({ step }) => {
+  return (
+    <>
+      {/* one of these is alternately hidden, depending on media query */}
+      <Col
+        className="col-steps-lg"
+        span={0}
+        md={11}
+      >
+        <Row
+          className="header-row-row-col2-row"
+          align="middle"
+        >
+          <Steps step={step} />
+        </Row>
+      </Col>
+      {/* one of these is alternately hidden, depending on media query */}
+      <Col
+        className="col-steps-sm"
+        span={0}
+        sm={7}
+        md={0}
+      >
+        <Row
+          className="header-row-row-col2-row"
+          align="middle"
+        >
+          <Steps step={step} />
+        </Row>
+      </Col>
+    </>
+  )
+}
+export default ResponsiveSteps
