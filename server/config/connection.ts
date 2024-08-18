3320d23739bb9f6b153b7b7b4f12a710686207c7
Leo Wong <leoelicos@gmail.com>
2024-08-18 23:43:04 +1000

    Suppress mongo deprecation warning

--- server/config/connection.ts
+++ server/config/connection.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
+mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ingre');
-
 export default mongoose.connection;
